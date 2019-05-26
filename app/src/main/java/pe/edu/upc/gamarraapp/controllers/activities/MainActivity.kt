4177ca52702f6f494fb077297c0f95d5cd7627d4
rodcar <ivanrod12@hotmail.com>
2019-05-26 13:24:27 -0500

    Agrega configuración de SearchView

--- app/src/main/java/pe/edu/upc/gamarraapp/controllers/activities/MainActivity.kt
+++ app/src/main/java/pe/edu/upc/gamarraapp/controllers/activities/MainActivity.kt
@@ -1,7 +1,11 @@
 package pe.edu.upc.gamarraapp.controllers.activities
 
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
+import android.widget.SearchView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
@@ -46,5 +50,17 @@ class MainActivity : AppCompatActivity() {
             .beginTransaction()
             .replace(R.id.content, getFragmentFor(item))
             .commit() > 0
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+
+        // Associate searchable configuration with the SearchView
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        (menu.findItem(R.id.search).actionView as SearchView).apply {
+            setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        }
+
+        return true
     }
 }
