2b2ccd5fec5734ea175c5d22889e1661f560867d
rodcar <ivanrod12@hotmail.com>
2019-06-12 16:35:58 -0500

    Agrega Cloth Fragment

---
+++ app/src/main/java/pe/edu/upc/gamarraapp/controllers/activities/ClothActivity.kt
@@ -0,0 +1,13 @@
+package pe.edu.upc.gamarraapp.controllers.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pe.edu.upc.gamarraapp.R
+
+class ClothActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cloth)
+    }
+}
