fd517a1889041c19ce12b06f3b930ca633ce8ad5
rodcar <ivanrod12@hotmail.com>
2019-06-14 00:13:28 -0500

    Agrega añadir una prenda a la bolsa

--- app/src/main/java/pe/edu/upc/gamarraapp/adapters/ClotheAdapter.kt
+++ app/src/main/java/pe/edu/upc/gamarraapp/adapters/ClotheAdapter.kt
@@ -1,5 +1,6 @@
 package pe.edu.upc.gamarraapp.adapters
 
+import android.content.Context
 import android.content.Intent
 import android.util.Log
 import android.view.LayoutInflater
@@ -7,10 +8,17 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.Gson
 import kotlinx.android.synthetic.main.item_clothe.view.*
 import pe.edu.upc.gamarraapp.R
 import pe.edu.upc.gamarraapp.controllers.activities.ClothActivity
 import pe.edu.upc.gamarraapp.models.Clothe
+import pe.edu.upc.gamarraapp.network.GamarraApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class ClotheAdapter(var clothes: List<Clothe>) : RecyclerView.Adapter<ClotheAdapter.ViewHolder>() {
 
@@ -21,8 +29,16 @@ class ClotheAdapter(var clothes: List<Clothe>) : RecyclerView.Adapter<ClotheAdap
         val clothCard = itemView.clothItem
         // TODO Revisar la forma adecuada de inicializar la variable
         var clothId: Int = 5
+        lateinit var service: GamarraApi
 
         fun bindTo(clothe: Clothe) {
+            val retrofit: Retrofit = Retrofit.Builder()
+                .baseUrl("http://quiet-temple-50701.herokuapp.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+            service = retrofit.create<GamarraApi>(GamarraApi::class.java)
+
             clothId = clothe.id
             pictureImageView.apply {
                 setDefaultImageResId(R.mipmap.ic_launcher)
@@ -44,6 +60,21 @@ class ClotheAdapter(var clothes: List<Clothe>) : RecyclerView.Adapter<ClotheAdap
 
                 val toast = Toast.makeText(it.context, text, duration)
                 toast.show()
+                val sharedPref = itemView.context.getSharedPreferences("gamarra-app-shared-preferences", Context.MODE_PRIVATE)
+                val accessToken = sharedPref.getString("accessToken", "")
+                val userId = sharedPref.getInt("id", 0)
+                Log.d("BAG", accessToken)
+                service.saveMaker(userId, clothe, "Bearer ${accessToken}").enqueue(object: Callback<Object> {
+                    override fun onFailure(call: Call<Object>, t: Throwable) {
+                        t?.printStackTrace();
+                    }
+
+                    override fun onResponse(call: Call<Object>, response: Response<Object>) {
+                        Log.d("BAG", "Estado de la respuesta: ${response.code()}")
+                        var responseBody = response?.body()
+                        Log.i("BAG", Gson().toJson(responseBody))
+                    }
+                })
 
                 true
             }
