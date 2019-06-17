07496f6ef7d76b67ce5f7e546f10674c32ffecd7
rodcar <ivanrod12@hotmail.com>
2019-06-17 15:55:15 -0500

    Elimina el atributo birthdate de la data class User
    Se elimina el atributo birthdate de la data class User ya que el servicio tiene un problema al deserealizarlo debido al formato de la fecha. se debe buscar una manera de darle un formato usando anotaciones como @JsonFormat

--- app/src/main/java/pe/edu/upc/gamarraapp/models/User.kt
+++ app/src/main/java/pe/edu/upc/gamarraapp/models/User.kt
@@ -1,19 +1,23 @@
 package pe.edu.upc.gamarraapp.models
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import java.io.Serializable
 import java.util.*
 
 data class User (
-    val id: Int,
-    val firstName: String,
-    val secondName: String,
-    val fathersLastName: String,
-    val mothersLastName: String,
-    val dni: String,
-    val password: String,
-    val email: String,
-    val gender: Boolean,
-    val birthdate: Date
+    var id: Int,
+    var firstName: String,
+    var secondName: String,
+    var fathersLastName: String,
+    var mothersLastName: String,
+    var dni: String,
+    var password: String,
+    var email: String,
+    var gender: Boolean
+    // TODO El servicio tiene problemas al deserealizar la fecha, produce un error 400
+    // Usé @JsonFormat pero no se reconoció
+    //var birthdate: Date
 ) : Serializable {
-    constructor() : this(0,"","","","","","","",true,Date())
+
+    constructor() : this(0,"", "","","","","","",true)
 }
